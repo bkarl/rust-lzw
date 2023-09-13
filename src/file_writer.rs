749d71b4de65b54ad518dedb468e8f1c16edea38
bk <stereoklinke@googlemail.com>
2023-09-13 14:58:43 +0200

    tests

--- src/file_writer.rs
+++ src/file_writer.rs
@@ -1,27 +1,75 @@
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use std::fs::File;
 
-pub struct FileWriter {
-   file: File 
+pub struct FileWriter<W: std::io::Write> {
+    file: W,
 }
 
-impl FileWriter {
-    pub fn new(filename: &str) -> std::io::Result<FileWriter> {
-        Ok (FileWriter { file: File::create(filename)? })
+impl FileWriter<File> {
+    pub fn new(path: &str) -> std::io::Result<Self> {
+        let file = File::create(path)?;
+        Ok(FileWriter{ file: file })
     }
-    
+
+}
+
+impl<W: Write> FileWriter<W> {
     pub fn write_packed_contents_to_file(&mut self, data_to_write: &[u16]) -> io::Result<()> {
-        let mut last_char : u16 = 0;
+        let mut last_char: u16 = 0;
         let current_nof_bits = 9;
         let mut stitching_bit = 0;
         for code in data_to_write.iter() {
             let current_char = *code;
-            let byte_out : u8 = (current_char << stitching_bit) as u8 | (last_char >> (current_nof_bits - stitching_bit)) as u8;
+            let byte_out: u8 = (current_char << stitching_bit) as u8 | (last_char >> (current_nof_bits - stitching_bit)) as u8;
             self.file.write(&[byte_out])?;
             last_char = current_char;
             stitching_bit = (stitching_bit + current_nof_bits % u8::BITS) % u8::BITS;
         }
+        if stitching_bit != 0 {
+            self.file.write(&[(last_char >> (current_nof_bits - stitching_bit)) as u8])?;
+        }
         Ok(())
-    } 
+    }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockedFileWriter {
+        contents: Vec<u8>
+    }
+
+    impl Write for MockedFileWriter {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            self.contents.extend_from_slice(buf);
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            Ok(())           
+        }
+    }
+
+    macro_rules! writer_tests {
+        ($($name:ident: $data_in:expr,$data_out:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let mut mock_writer = Vec::new();
+                let mut writer_to_test = FileWriter {file: &mut mock_writer};
+                let data_in : &[u16] = &$data_in;
+                let data_out : &[u8] = &$data_out;
+                writer_to_test.write_packed_contents_to_file(data_in).unwrap();
+                assert_eq!(data_out, mock_writer);
+            }
+        )*
+        }
+    }
+
+    writer_tests! {
+        test_empty: [], [],
+        test_single: [1], [1, 0],
+        test_double: [1, 1], [1, 2, 0],
+    }}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 
 mod lz_dictionary;
 use lz_dictionary::{LzDictionary, EMPTY_PATTERN};
@@ -7,35 +7,36 @@ mod file_writer;
 use file_writer::FileWriter;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    let mut file_in = &File::open("file_in.bin")?; 
+    let mut file_in = &File::open("file_in.bin")?;
     let mut input_buffer = Vec::new();
     let mut compressor = LzCompressor::new();
     let mut file_writer = FileWriter::new("data/data_out.bin")?;
     file_in.read_to_end(&mut input_buffer)?;
- 
+
     let compressed_data = compressor.compress_data(input_buffer.as_ref());
     file_writer.write_packed_contents_to_file(&compressed_data)?;
     Ok(())
 }
 
 struct LzCompressor {
-    dictionary: LzDictionary
+    dictionary: LzDictionary,
 }
 
 impl LzCompressor {
     pub fn new() -> LzCompressor {
-        LzCompressor { dictionary: LzDictionary::new() }
+        LzCompressor {
+            dictionary: LzDictionary::new(),
+        }
     }
-    
+
     pub fn compress_data(&mut self, data_to_compress: &[u8]) -> Vec<u16> {
         let mut ret = Vec::new();
-        let mut current_pattern :  u16 = EMPTY_PATTERN;
+        let mut current_pattern: u16 = EMPTY_PATTERN;
         for byte in data_to_compress {
             let new_pattern = (current_pattern, *byte as u16);
             if let Some(char_idx) = self.dictionary.is_pattern_in_dictionary(new_pattern) {
                 current_pattern = char_idx;
-            }
-            else {
+            } else {
                 self.dictionary.add_pattern_to_dictionary(new_pattern);
                 ret.push(current_pattern);
                 current_pattern = *byte as u16;
